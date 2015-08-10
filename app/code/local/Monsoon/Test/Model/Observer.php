eb3a2075d17f0a75019407ccd9cca9912ae6ab49
hardanro <hardanro@yahoo.com>
2015-08-11 02:23:29 +0300

    Performance improvments

--- app/code/local/Monsoon/Test/Model/Observer.php
+++ app/code/local/Monsoon/Test/Model/Observer.php
@@ -6,6 +6,8 @@ class Monsoon_Test_Model_Observer extends Mage_Catalog_Model_Observer {
 
     const NOLINK = 'javascript:;';
 
+    protected $unClickableIds;
+
     /**
      * customizeTopMenu observer - called on page_block_html_topmenu_gethtml_before in config.xml
      *
@@ -15,6 +17,15 @@ class Monsoon_Test_Model_Observer extends Mage_Catalog_Model_Observer {
     public function customizeTopMenu(Varien_Event_Observer $observer) {
         $block = $observer->getEvent()->getBlock();
         $block->addCacheTag(Mage_Catalog_Model_Category::CACHE_TAG);
+
+        //Get collection of unclickable category ids in the menu
+        if(null !== Mage::getModel('catalog/resource_eav_attribute')->loadByCode('catalog_category','unclickable_menu_category')->getId()) {
+            $collection = Mage::getModel('catalog/category')->getCollection();
+            $collection->addAttributeToSelect('*');
+            $collection->addAttributeToFilter('unclickable_menu_category', array('eq' => 1));
+            $this->unClickableIds = $collection->getAllIds();
+        }
+
         $this->_addCategoriesToMenu(
             Mage::helper('catalog/category')->getStoreCategories(), $observer->getMenu(), $block, true
         );
@@ -32,6 +43,8 @@ class Monsoon_Test_Model_Observer extends Mage_Catalog_Model_Observer {
 
     protected function _addCategoriesToMenu($categories, $parentCategoryNode, $menuBlock, $addTags = false)
     {
+        //Get collection of unclickable category ids in the menu
+
         $categoryModel = Mage::getModel('catalog/category');
         foreach ($categories as $category) {
             if (!$category->getIsActive()) {
@@ -47,14 +60,11 @@ class Monsoon_Test_Model_Observer extends Mage_Catalog_Model_Observer {
 
             $tree = $parentCategoryNode->getTree();
 
-            //See if the category is set as unclickable in admin
-            $unClickable = Mage::getModel('catalog/category')->load($category->getId())->getData('unclickable_menu_category');
-
             //Set javascript:; as category url if category attribute is unclickable
             $categoryData = array(
                 'name' => $category->getName(),
                 'id' => $nodeId,
-                'url' => !empty($unClickable) ? (self::NOLINK) : Mage::helper('catalog/category')->getCategoryUrl($category),
+                'url' => in_array($category->getId(),$this->unClickableIds) ? (self::NOLINK) : Mage::helper('catalog/category')->getCategoryUrl($category),
                 'is_active' => $this->_isActiveMenuCategory($category)
             );
 
